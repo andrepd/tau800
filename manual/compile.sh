108fb73a5fbadf8b91e553a091b04d5dedbf50ae
mikeevmm <miguelmurca@gmail.com>
2021-09-18 17:07:01 +0100

    tex: compile.sh creates fake scan

--- manual/compile.sh
+++ manual/compile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e # abort on any error
 mkdir -p out
 latexmk -output-directory=out -pdf document.tex $@
+convert -density 200 out/document.pdf -rotate 0.1 +noise Multiplicative -format pdf -quality 85 -compress JPEG -colorspace gray out/document-scan.pdf
