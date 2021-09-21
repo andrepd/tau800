45455617eb9ae3209382cacc93f2fd2f80cef71b
mikeevmm <miguelmurca@gmail.com>
2021-09-21 23:28:13 +0100

    backend: refactor: word types

--- interpreter/src/basics.rs
+++ interpreter/src/basics.rs
@@ -1,24 +1,112 @@
-/// Aqui temos de assegurar que aritmética, etc é modulo 2^6. 
-/// Crates que há para ter um u6 / u12 não são práticas de usar
-const CHAR_BIT: usize = 6;
-pub type Word = u8;
-pub type Address = u16;
-/// Lá está, aqui é chato. Como converter um u6 para i6? Tem de ser à mão. 
-pub type SWord = i8;  // Signed and same size as Word
+use std::{marker::PhantomData, ops::Add};
 
-pub const MAX_WORD: usize = 1 << CHAR_BIT;
-pub const RAM_SIZE: usize = 1 << (2*CHAR_BIT);
+const WORD_SIZE: usize = 6;
+const MAX_UNSIGNED_VALUE: u8 = 1 << WORD_SIZE;
 
+pub mod word {
+    pub trait Signature {}
 
+    pub type Signed = i8;
+    pub type Unsigned = u8;
 
-pub fn word_to_sword(x: Word) -> SWord {
-  debug_assert!((x as usize) < MAX_WORD);
-  const HALF_MAX: usize = 1 << (CHAR_BIT - 1);
-  if x < (HALF_MAX - 1) as Word {
-    x as SWord
-  } else {
-    // Sanity check pls, also não sei fazer testes em rust
-    /*-(MAX_WORD as Word - x) as SWord*/
-    -((MAX_WORD - x as usize) as isize) as SWord  // Jfc
-  }
+    impl Signature for Signed {}
+    impl Signature for Unsigned {}
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct Word<X>
+where
+    X: word::Signature,
+{
+    value: u8,
+    phantom: PhantomData<X>,
+}
+
+impl Word<word::Unsigned> {
+    pub fn value(&self) -> u8 {
+        self.value
+    }
+}
+
+impl Word<word::Signed> {
+    pub fn value(&self) -> i8 {
+        self.value as i8
+    }
+}
+
+impl From<u8> for Word<word::Unsigned> {
+    fn from(value: u8) -> Self {
+        debug_assert!(value < MAX_UNSIGNED_VALUE);
+        Word {
+            value,
+            phantom: PhantomData::default(),
+        }
+    }
+}
+
+impl From<Word<word::Unsigned>> for u8 {
+    fn from(word: Word<word::Unsigned>) -> Self {
+        word.value
+    }
+}
+
+impl From<i8> for Word<word::Signed> {
+    fn from(value: i8) -> Self {
+        debug_assert!((value as u8) < MAX_UNSIGNED_VALUE);
+        Word {
+            value: (value as u8),
+            phantom: PhantomData::default(),
+        }
+    }
+}
+
+impl From<Word<word::Signed>> for i8 {
+    fn from(word: Word<word::Signed>) -> Self {
+        word.value as i8
+    }
+}
+
+impl<X> Default for Word<X>
+where
+    X: word::Signature + Default,
+{
+    fn default() -> Self {
+        Self {
+            value: Default::default(),
+            phantom: PhantomData::default(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct LongWord<S>
+where
+    S: word::Signature,
+{
+    high: Word<S>,
+    low: Word<S>,
+}
+
+impl LongWord<word::Unsigned> {
+    pub fn from_words(high: Word<word::Unsigned>, low: Word<word::Unsigned>) -> Self {
+        Address { high, low }
+    }
+}
+
+impl Into<u16> for LongWord<word::Unsigned> {
+    fn into(self) -> u16 {
+        (u8::from(self.low) as u16) + ((u8::from(self.high) as u16) << WORD_SIZE)
+    }
+}
+
+impl Default for LongWord<word::Unsigned> {
+    fn default() -> Self {
+        Self {
+            high: Default::default(),
+            low: Default::default(),
+        }
+    }
+}
+
+pub type UnsignedLongWord = LongWord<word::Unsigned>;
+pub type Address = LongWord<word::Unsigned>;
