383d7539fe05ee8ba88c9dbaebb527df3ceee9c5
mikeevmm <miguelmurca@gmail.com>
2021-09-30 23:49:36 +0100

    interp: 0x10 as module source

--- interpreter/src/modules.rs
+++ interpreter/src/modules.rs
@@ -24,7 +24,7 @@ impl<'a> ModuleCollection<'a> {
     }
 
     pub fn run(&mut self, universe: &mut Universe) {
-        let mut slice_start = 0x10 << 6;
+        let mut slice_start = 0x10;
         for module in self.0.iter_mut() {
             let slice_length = module.size();
             let memmap = &mut universe.now_mut().ram.0[slice_start..(slice_start + slice_length)];
