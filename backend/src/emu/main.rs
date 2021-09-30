05035466fb4dfa1a975300208addda24aab41dc0
mikeevmm <miguelmurca@gmail.com>
2021-09-30 20:35:11 +0100

    backend: symlinked

---
+++ backend/src/emu/main.rs
@@ -0,0 +1,1 @@
+/home/miguelmurca/Documents/Art/PDC2021/repo/backend/../interpreter/src/main.rs
--- backend/src/emu/prelude.rs
+++ backend/src/emu/prelude.rs
@@ -1,12 +1,1 @@
-pub(super) use super::word::*;
-pub(super) use super::state::*;
-pub(super) use super::machine::Machine;
-pub(super) use super::instruction::{Instruction, Op, Operand, Operands, Register};
-pub(super) use super::universe::Universe;
-
-/// Tou sempre a ter de usar isto
-pub fn div_rem<
-  T: Copy + std::ops::Div<Output = T> + std::ops::Rem<Output = T>
->(a: T, b: T) -> (T, T) {
-  (a / b, a % b)
-}
+/home/miguelmurca/Documents/Art/PDC2021/repo/backend/../interpreter/src/prelude.rs
